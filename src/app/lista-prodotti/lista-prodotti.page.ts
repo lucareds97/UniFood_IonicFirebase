16a4fd9e903c61f37fbefcd7557b1ce44071025d
lucareds97 <2110luca1@gmail.com>
2019-05-23 09:18:50 +0200

    1) Gestito collegamento tra LIsta Prodotti Page e Nuovo Prodotto Page 2) Aggiunto bottone Back su Nuovo Prodotto Page 3) Aggiunti controlli in console sui campi inseriti del nuovo prodotto (da sostituire con alert possibilmente) 4) Gestita lista Prodotti Page, prende i dati ritornati dal metodo getProducts, stampa il numero esatto di prodotti in base a quelli presenti in "database" ma stampa sempre lo stesso prodotto.. Da Risolvere

--- src/app/lista-prodotti/lista-prodotti.page.ts
+++ src/app/lista-prodotti/lista-prodotti.page.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProductsService } from '../services/products.service';
 import { Router } from '@angular/router';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { Prodotto } from '../interfaces/prodotti';
 
 @Component({
   selector: 'app-lista-prodotti',
   templateUrl: 'lista-prodotti.page.html',
   styleUrls: ['lista-prodotti.page.scss']
 })
-export class ListaProdottiPage {
+export class ListaProdottiPage implements OnInit{
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
+
+  i: number = 0;
+  listaProdotti: any[] = [];
+
+  prodotto: Prodotto = {
+    nome: '',
+    descrizione: '',
+    prezzo: '',
+    linkImmagine: '',
+
+  };
 
   constructor(private prodService: ProductsService, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.prodService.getProducts().subscribe(res => {
+      //console.log(res);
+
+      res.forEach(id => {
+
+        this.prodotto.nome = res[this.i]['nome'];
+        this.prodotto.descrizione = res[this.i]['descrizione'];
+        this.prodotto.prezzo = res[this.i]['prezzo'];
+        this.prodotto.linkImmagine = res[this.i]['linkImmagine'];
+        
+        this.listaProdotti.push(res[this.i]);
+
+        console.log("ITERATA" + this.i);
+        console.log(this.listaProdotti[this.i]);
+
+        this.i++;
+
+      });
+
+    });
   }
 
   aggiungiNuovoProdotto(){
@@ -17,4 +55,3 @@ export class ListaProdottiPage {
   }
 
 }
-
--- src/app/nuovo-prodotto/nuovo-prodotto.page.ts
+++ src/app/nuovo-prodotto/nuovo-prodotto.page.ts
@@ -1,33 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../services/products.service';
 import { Prodotto } from '../interfaces/prodotti';
+import { AlertController } from '@ionic/angular';
+import { PopoverController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nuovo-prodotto',
   templateUrl: 'nuovo-prodotto.page.html',
   styleUrls: ['nuovo-prodotto.page.scss']
 })
-export class NuovoProdottoPage implements OnInit {
+export class NuovoProdottoPage{
 
   prodotto: Prodotto = {
     nome: '',
     descrizione: '',
     prezzo: '',
     linkImmagine: '',
-
   };
 
-  constructor(private prodService: ProductsService) {
+  constructor(private prodService: ProductsService, public alertController: AlertController, private router: Router) {
    }
 
-  ngOnInit() {
-    this.prodService.getProducts().subscribe(res => {
-      console.log(res);
-    });
-  }
 
   inserisciProdotto(){
+    if(this.prodotto.nome == '' || this.prodotto.descrizione == '' || this.prodotto.prezzo == '' || this.prodotto.nome == ''){
+      console.log("INSERIRE TUTTI I CAMPI!");
+    }else{
     this.prodService.addProduct(this.prodotto);
+    this.router.navigateByUrl("/lista-prodotti");
   }
+}
 
 }
